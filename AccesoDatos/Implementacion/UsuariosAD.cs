5ac80756fa04e6194183e0d09772fed472e3ba11
DenisRodMor <104526631+DenisRodMor@users.noreply.github.com>
2022-06-21 10:20:43 -0600

    Agregar archivos de proyecto.

---
+++ AccesoDatos/Implementacion/UsuariosAD.cs
@@ -0,0 +1,80 @@
+﻿using Entidades;
+using Microsoft.Extensions.Logging;
+using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccesoDatos.Implementacion
+{
+   public class UsuariosAD
+    {
+
+        private readonly Logger gobjLogger = LogManager.GetCurrentClassLogger();
+        private CapasEntidadesss gObjContextoUser;
+
+        public UsuariosAD(CapasEntidadesss lgObjContextoUser)
+        {
+            gObjContextoUser = lgObjContextoUser;
+        }
+
+        public List<PA_ListadoUsuarios_Result> recUsuarios_PA()
+        {
+            List<PA_ListadoUsuarios_Result> lObjRespuesta = new List<PA_ListadoUsuarios_Result>();
+            try
+            {
+                lObjRespuesta = gObjContextoUser.PA_ListadoUsuarios().ToList();
+            }
+            catch (Exception ex)
+            {
+                gobjLogger.Error("Se produjo un error. Detalle " + ex.Message + " " + ex.InnerException.Message);
+                throw ex;
+            }
+            return lObjRespuesta;
+        }
+
+        public bool insUsuarios_PA(Usuarios pUsuarios)
+        {
+            bool lObjRespuesta = false;
+            int intValor = 0;
+
+            try
+            {
+                intValor = gObjContextoUser.PA_InsertarUsuario_( pUsuarios.Nombre, pUsuarios.Apellidos, pUsuarios.Usuario, pUsuarios.Clave , pUsuarios.Direccion, pUsuarios.Telefono, pUsuarios.TipodeUsuario);
+                if (intValor == 1)
+                {
+                    lObjRespuesta = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                gobjLogger.Error("Se produjo un error. Detalle " + ex.Message + " " + ex.InnerException.Message);
+                throw ex;
+            }
+            return lObjRespuesta;
+        }
+
+        public bool modUsuarios_PA(Usuarios pUsuarios)
+        {
+            bool lObjRespuesta = false;
+            int intValor = 0;
+
+            try
+            {
+                intValor = gObjContextoUser.PA_EditarUsuarios(pUsuarios.Id ,pUsuarios.Nombre, pUsuarios.Apellidos ,pUsuarios.Usuario, pUsuarios.Clave, pUsuarios.Direccion, pUsuarios.Telefono, pUsuarios.TipodeUsuario);
+                if (intValor == 1)
+                {
+                    lObjRespuesta = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                gobjLogger.Error("Se produjo un error. Detalle " + ex.Message + " " + ex.InnerException.Message);
+                throw ex;
+            }
+            return lObjRespuesta;
+        }
+    }
+}
---
+++ AccesoDatos/Interfaces/IUsuariosAD.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entidades;
+
+namespace AccesoDatos.Interfaces
+{
+   public interface IUsuariosAD
+    {
+
+        List<PA_ListadoUsuarios_Result> recUsuarios_PA();
+        bool insUsuarios_PA(Usuarios pUsuarios);
+        bool modUsuarios_PA(Usuarios pUsuarios);
+        bool delUsuarios_PA(Usuarios pUsuarios);
+
+    }
+}
